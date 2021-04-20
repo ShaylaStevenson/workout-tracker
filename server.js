583d40046e35df4cd6b0ed6fcdb3059fba7ff5f7
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-04-20 13:22:50 -0700

    deploying

--- server.js
+++ server.js
@@ -13,8 +13,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-//note: workoutTracker
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTracker", { useNewUrlParser: true });
+//db name: workoutTracker
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workoutTracker',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 
 //routes
 app.use(require("./routes/apiRoutes.js"));
