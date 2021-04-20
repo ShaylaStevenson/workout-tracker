1e99c2d4847ac50619402a538ab58f798d9f9645
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-04-20 10:41:38 -0700

    fixed html routes, loading

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,17 +4,17 @@ const path = require('path');
 
 // homepage: get '/', /index.html 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "../public/index.html"));
+    res.sendFile(path.join(__dirname + "../../public/index.html"));
 });
 
 // exercise page: get '/exercise', /exercise.html
 router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname + "../public/exercise.html"));
+    res.sendFile(path.join(__dirname + "../../public/exercise.html"));
 });
 
 // stats page: get '/stats', /stats.html
 router.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname + "../public/stats.html"));
+    res.sendFile(path.join(__dirname + "../../public/stats.html"));
 });
 
 module.exports = router;
