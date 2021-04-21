4ef1270b9870f8163f0647dbe1de9509b792d179
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-04-21 11:29:41 -0700

    get all workouts route displaying right in console

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,20 @@
 const db = require('../models');
 const router = require('express').Router();
 
-    // get all  workoutes -GET
+// get all  workoutes -GET
+router.get("/api/workouts", (req, res) => {
+
+    db.Workout.find({}).then(allWorkouts => {
+        //console.log("all workouts");
+        //console.log(allWorkouts);
+        res.json(allWorkouts);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+
+
     // create new workut -POST
     // add exercise to a workout -PUT
 
